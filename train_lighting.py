4437f2e9bd69c4b4a6178baeff15f9aea76a2fc7
Joschka Birk <joschka.birk@gmail.com>
2025-09-29 21:31:21 +0000

    Fix that --use_pid and --use_add work as expected

--- train_lighting.py
+++ train_lighting.py
@@ -11,6 +11,18 @@ from lightining_model import PETLightning
 from utils import TrainLossEarlyStopping, get_latest_checkpoint_dir
 
 
+# https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ("yes", "true", "t", "y", "1"):
+        return True
+    elif v.lower() in ("no", "false", "f", "n", "0"):
+        return False
+    else:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Train PET model in PyTorch Lightning")
 
@@ -79,8 +91,8 @@ def main():
     )
 
     # Additional features
-    parser.add_argument("--use_pid", type=bool, default=True)
-    parser.add_argument("--use_add", type=bool, default=True)
+    parser.add_argument("--use_pid", type=str2bool, default=False)
+    parser.add_argument("--use_add", type=str2bool, default=False)
 
     args = parser.parse_args()
 
