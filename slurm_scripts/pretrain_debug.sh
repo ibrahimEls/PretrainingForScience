dd1b64d40da3f844b189cf6ca17f7a92b38f57d7
Ibrahim Elsharkawy <ibrahime@login20.chn.perlmutter.nersc.gov>
2025-10-02 12:17:46 -0700

    Wandb Fix

--- slurm_scripts/pretrain_debug.sh
+++ slurm_scripts/pretrain_debug.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 #SBATCH -A m4287
 #SBATCH -C gpu
-#SBATCH -q regular
-#SBATCH -t 10:00:00
-#SBATCH -N 10
+#SBATCH -q debug
+#SBATCH -t 00:30:00
+#SBATCH -N 8
 #SBATCH --ntasks-per-node=4
 #SBATCH -c 32
 #SBATCH --gpus-per-task=1
@@ -12,4 +12,4 @@
 module load conda
 conda activate torchvenv
 cd /global/homes/i/ibrahime/FoundationModelStudy/OmnilearnLightning
-srun --gpus-per-node 4 python3 train_lighting.py --dataset_size=100_000_000 --num_workers=32 --num_nodes=1 --model_size=small --use_pid y --use_add y --epoch=1 --use_wandb
+srun --gpus-per-node 4 python3 train_lighting.py --dataset_size=100_000_000 --num_workers=32 --num_nodes=8 --model_size=small --use_pid y --use_add y --epoch=1 --use_wandb
