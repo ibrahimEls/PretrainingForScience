02300c2bbf9dfaab1fc8799a298d739becd703fb
Ibrahim Elsharkawy <ibrahime@login20.chn.perlmutter.nersc.gov>
2025-10-02 12:46:15 -0700

    MultiNode Fix

--- slurm_scripts/pretrain_debug.sh
+++ slurm_scripts/pretrain_debug.sh
@@ -14,7 +14,7 @@ conda activate torchvenv
 cd /global/homes/i/ibrahime/FoundationModelStudy/OmnilearnLightning
 
 ## Small Model
-#srun --gpus-per-node 4 python3 train_lightning.py --dataset_size=100_000_000 --num_workers=32 --num_nodes=8 --model_size=small --use_pid y --use_add y --epoch=1 --use_wandb --batch_size=128
+srun --gpus-per-node 4 python3 train_lightning.py --dataset_size=100_000_000 --num_workers=32 --num_nodes=8 --model_size=small --use_pid y --use_add y --epoch=1 --use_wandb --batch_size=128
 
 ## Medium Model
-srun --gpus-per-node 4 python3 train_lightning.py --dataset_size=100_000_000 --num_workers=32 --num_nodes=8 --model_size=medium --use_pid y --use_add y --epoch=1 --use_wandb --batch_size=32
+#srun --gpus-per-node 4 python3 train_lightning.py --dataset_size=100_000_000 --num_workers=32 --num_nodes=8 --model_size=medium --use_pid y --use_add y --epoch=1 --use_wandb --batch_size=32
