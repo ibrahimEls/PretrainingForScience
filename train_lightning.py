f59954716d7e78b2a73ab524d8f4d77e1a576756
Joschka Birk <joschka.birk@gmail.com>
2025-10-03 00:06:28 +0000

    Only create directory on rank 0

--- train_lightning.py
+++ train_lightning.py
@@ -6,6 +6,7 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import CSVLogger, WandbLogger
 from pytorch_lightning.strategies import DDPStrategy
+from pytorch_lightning.utilities import rank_zero_only
 
 from dataloader import PETDataModule
 from lightning_model import PETLightning
@@ -138,8 +139,9 @@ def main():
         lr = 1e-5
         save_tag = f"super_gen_medium_{args.dataset_size}"
 
-    # Create output directory
-    os.makedirs(args.outdir, exist_ok=True)
+    # Create output directory only on rank 0
+    if rank_zero_only.rank == 0:
+        os.makedirs(args.outdir, exist_ok=True)
 
     batch_size = args.batch_size
 
@@ -204,7 +206,8 @@ def main():
     run_name = f"v{version}_{get_bigram(add_timestamp=True)}"
 
     run_dir = os.path.join(out_dir_save_tag, run_name)
-    os.makedirs(run_dir, exist_ok=True)
+    if rank_zero_only.rank == 0:
+        os.makedirs(run_dir, exist_ok=True)
 
     print(f"Output directory of this run: {run_dir}")
 
