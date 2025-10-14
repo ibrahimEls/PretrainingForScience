aa570aa542b6da63202a14b8a11c2b32ccd40e22
Joschka Birk <joschka.birk@gmail.com>
2025-10-14 21:36:59 +0000

    Add targets (token-resolution masked particles) to scatter plots and add residual plots

--- omnilearn_lightning/callbacks/masked_prediction_callback.py
+++ omnilearn_lightning/callbacks/masked_prediction_callback.py
@@ -9,7 +9,7 @@ import torch
 import wandb
 from pytorch_lightning import Callback, LightningModule
 
-from omnilearn_lightning.array_utils import np_to_ak
+from omnilearn_lightning.array_utils import ak_subtract, np_to_ak
 from omnilearn_lightning.plotting.feature_plotting import plot_features
 from omnilearn_lightning.plotting.utils import set_mpl_style
 
@@ -53,10 +53,9 @@ class MaskedPredictionCallback(Callback):
         }
 
         # Prepare awkward arrays for all batches
-        # all_batch_ak = []
-        # all_batch_tokenized_ak = []
         batches_survived_ak = []
         batches_masked_pred_ak = []
+        batches_masked_true_tokenized_ak = []
         batches_masked_true_ak = []
         batches_jet_labels = []
 
@@ -68,18 +67,21 @@ class MaskedPredictionCallback(Callback):
             ].bool()
             batch_tokenized[~mask_valid_particle] = 0  # set invalid points to 0
 
-            # predicted_tokens = output["masked_pred"].argmax(dim=-1)
-            probs = torch.nn.functional.softmax(output["masked_pred"], dim=-1)
-            # reshape to (batch_size * num_points, num_tokens)
-            probs = probs.view(-1, probs.shape[-1])
-            # sample once from the categorical distribution
-            predicted_tokens = torch.multinomial(probs, num_samples=1).squeeze(-1)
-            predicted_tokens = predicted_tokens.view(output["masked_pred"].shape[0], -1)
-            reconstructed = pl_module.tokenizer.kmeans.centroids[predicted_tokens]
+            predicted_tokens = output["masked_pred"].argmax(dim=-1)
+            # probs = torch.nn.functional.softmax(output["masked_pred"], dim=-1)
+            # # reshape to (batch_size * num_points, num_tokens)
+            # probs = probs.view(-1, probs.shape[-1])
+            # # sample once from the categorical distribution
+            # predicted_tokens = torch.multinomial(probs, num_samples=1).squeeze(-1)
+            # predicted_tokens = predicted_tokens.view(output["masked_pred"].shape[0], -1)
+            predictions_reconstructed = pl_module.tokenizer.kmeans.centroids[
+                predicted_tokens
+            ]
+            targets_reconstructed = pl_module.tokenizer.transform(batch["X"])
 
             batches_masked_pred_ak.append(
                 np_to_ak(
-                    reconstructed.cpu().numpy(),
+                    predictions_reconstructed.cpu().numpy(),
                     names=feature_names.keys(),
                     mask=mask_valid_particle_but_masked.cpu().numpy(),
                 )
@@ -100,12 +102,21 @@ class MaskedPredictionCallback(Callback):
                     .numpy(),
                 )
             )
+            batches_masked_true_tokenized_ak.append(
+                np_to_ak(
+                    batch_tokenized.cpu().numpy(),
+                    names=feature_names.keys(),
+                    mask=mask_valid_particle_but_masked.cpu().numpy(),
+                )
+            )
+
             batches_jet_labels.append(batch["y"].cpu().numpy())
 
         # Concatenate awkward arrays
         x_part_ak_pred = ak.concatenate(batches_masked_pred_ak)
         x_part_ak_true = ak.concatenate(batches_masked_true_ak)
         x_part_ak_survived = ak.concatenate(batches_survived_ak)
+        x_part_ak_true_tokenized = ak.concatenate(batches_masked_true_tokenized_ak)
         jet_labels = np.concatenate(batches_jet_labels)
 
         # shuffle the jets
@@ -113,8 +124,22 @@ class MaskedPredictionCallback(Callback):
         x_part_ak_pred = x_part_ak_pred[shuffle_idx]
         x_part_ak_true = x_part_ak_true[shuffle_idx]
         x_part_ak_survived = x_part_ak_survived[shuffle_idx]
+        x_part_ak_true_tokenized = x_part_ak_true_tokenized[shuffle_idx]
         jet_labels = jet_labels[shuffle_idx]
 
+        def save_and_log(fig, name):
+            filename = (
+                f"/tmp/mpm_visualization_epoch_{name}_step{trainer.global_step:06d}.png"
+            )
+            fig.savefig(filename, dpi=300)
+            for logger in trainer.loggers:
+                if logger.__class__.__name__ == "WandbLogger":
+                    print(f"Logging image {filename} to wandb")
+                    logger.experiment.log(
+                        {f"mpm_visualization_{name}": wandb.Image(filename)}
+                    )
+
+        # compare distributions of predicted vs true masked vs survived particles
         fig, axarr = plot_features(
             {
                 "Survived": x_part_ak_survived,
@@ -130,32 +155,35 @@ class MaskedPredictionCallback(Callback):
             },
             ratio=True,
         )
-
-        def save_and_log(fig, name):
-            filename = (
-                f"/tmp/mpm_visualization_epoch_{name}_step{trainer.global_step:06d}.png"
-            )
-            fig.savefig(filename, dpi=400)
-            for logger in trainer.loggers:
-                if logger.__class__.__name__ == "WandbLogger":
-                    logger.experiment.log(
-                        {f"mpm_visualization_{name}": wandb.Image(filename)}
-                    )
-                else:
-                    print(
-                        f"Logger {logger.__class__.__name__} not supported for image logging."
-                    )
-
         save_and_log(fig, "particle_distributions")
+
+        # plot residuals of predicted vs true masked particles
+        fig, axarr = plot_features(
+            {
+                "Predicted - True Masked": ak_subtract(x_part_ak_pred, x_part_ak_true),
+                "Predicted - True Masked (Tokenized)": ak_subtract(
+                    x_part_ak_pred, x_part_ak_true_tokenized
+                ),
+            },
+            names=feature_names,
+            bins_dict={
+                "eta": np.linspace(-1, 1, 50),
+                "phi": np.linspace(-1, 1, 50),
+                "log_pt": np.linspace(-3, 3, 50),
+                "log_E": np.linspace(-3, 3, 50),
+            },
+        )
+        save_and_log(fig, "particle_residuals")
 
         # ------ scatter plots for a few example jets ------
 
-        fig, ax = plt.subplots(2, 8, figsize=(20, 5.5))
+        fig, ax = plt.subplots(3, 8, figsize=(20, 7.5))
 
         # ax = ax.flatten()
         for i_jet in range(8):
             ax_top = ax[0, i_jet]
-            ax_bottom = ax[1, i_jet]
+            ax_middle = ax[1, i_jet]
+            ax_bottom = ax[2, i_jet]
 
             ax_top.scatter(
                 x_part_ak_survived[i_jet].phi,
@@ -172,6 +200,23 @@ class MaskedPredictionCallback(Callback):
                 c="C1",
                 alpha=0.5,
                 label="Masked",
+            )
+
+            ax_middle.scatter(
+                x_part_ak_survived[i_jet].phi,
+                x_part_ak_survived[i_jet].eta,
+                s=np.exp(x_part_ak_survived[i_jet].log_pt) * 1,
+                c="C0",
+                alpha=0.5,
+                label="Survived",
+            )
+            ax_middle.scatter(
+                x_part_ak_true_tokenized[i_jet].phi,
+                x_part_ak_true_tokenized[i_jet].eta,
+                s=np.exp(x_part_ak_true_tokenized[i_jet].log_pt) * 1,
+                c="C3",
+                alpha=0.5,
+                label="Masked (tokenized)",
             )
 
             ax_bottom.scatter(
@@ -191,13 +236,11 @@ class MaskedPredictionCallback(Callback):
                 label="Predicted",
             )
 
-            # label
+            # jet type label
             jet_label = jet_labels[i_jet]
 
-            ax_top.set_title(f"Jet {i_jet + 1}, $label={jet_label}$", fontsize=10)
-            ax_bottom.set_title(f"Jet {i_jet + 1}, $label={jet_label}$", fontsize=10)
-
-            for _ax in [ax_top, ax_bottom]:
+            for _ax in [ax_top, ax_middle, ax_bottom]:
+                _ax.set_title(f"Jet {i_jet + 1}, $label={jet_label}$", fontsize=10)
                 _ax.set_xlim(-1, 1)
                 _ax.set_ylim(-1, 1.4)
                 _ax.set_xlabel(feature_names["phi"])
