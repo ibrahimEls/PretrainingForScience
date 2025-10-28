6558d0175bd94d9799a8c68c4a34d3a6364a3df8
Joschka Birk <joschka.birk@gmail.com>
2025-10-28 03:55:39 +0000

    Fix indentation in MPM shared step

--- omnilearn_lightning/model.py
+++ omnilearn_lightning/model.py
@@ -517,9 +517,9 @@ class PETLightning(LightningModule):
                 self.tokenizer.kmeans.centroids = self.tokenizer.kmeans.centroids.to(
                     X.device
                 )
-                model_kwargs["masking_fraction"] = self.hparams.masking_fraction
+            model_kwargs["masking_fraction"] = self.hparams.masking_fraction
 
-                y_masked = self.tokenizer.predict(X, add_info=model_kwargs["add_info"])
+            y_masked = self.tokenizer.predict(X, add_info=model_kwargs["add_info"])
 
         # Use torch.no_grad() for validation and test, allow gradients for training
         if stage == "train":
