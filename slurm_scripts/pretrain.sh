b88b4b955ba1a3c5777ebdad56797ea78e19a130
Joschka Birk <joschka.birk@gmail.com>
2025-11-07 02:13:43 +0000

    Update batch size and wandb project name

--- slurm_scripts/pretrain.sh
+++ slurm_scripts/pretrain.sh
@@ -23,10 +23,10 @@ DATASET_PATH=/pscratch/sd/j/jobirk/omnilearn_datasets/
 # ------------------------------------
 
 TOKENIZER_CKPT="${REPO_DIR}/assets/kmeans_model_2025-11-01-00-13-29-Idiotic-Name_withAddInfo_16384_codes.pth"
-TRAINING_CFG_ALL_SIZES="--use_wandb y --wandb_project omnilearn --limit_val_batches=50 --val_check_interval=2000 --dataset_size=-1 --use_pid y --use_add y --epoch=500  --outdir=$OUTPUT_DIR --path=$DATASET_PATH --shuffle_val_test_indices y --seed_for_initial_shuffling=1603"
+TRAINING_CFG_ALL_SIZES="--use_wandb y --wandb_project omnilearned --limit_val_batches=50 --val_check_interval=2000 --dataset_size=-1 --use_pid y --use_add y --epoch=500  --outdir=$OUTPUT_DIR --path=$DATASET_PATH --shuffle_val_test_indices y --seed_for_initial_shuffling=1603"
 
 ### Micro Model
-TRAINING_CFG_SIZE_SPECIFIC="--num_workers=2 --num_nodes=4 --model_size=micro --lr 1e-3 --weight_decay 0.01 --batch_size=256"
+TRAINING_CFG_SIZE_SPECIFIC="--num_workers=2 --num_nodes=4 --model_size=micro --lr 1e-3 --weight_decay 0.01 --batch_size=128"
 ### Small model
 # TRAINING_CFG_SIZE_SPECIFIC="--num_workers=32 --num_nodes=8 --model_size=small --lr 5e-4 --weight_decay 0.3 --batch_size=128"
 ### Medium model
