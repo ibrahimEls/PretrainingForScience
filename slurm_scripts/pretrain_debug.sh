ef25f695c56f9d5a0071baae1abf9b2958a97d90
Joschka Birk <joschka.birk@gmail.com>
2025-11-12 01:35:23 +0000

    Dynamically adapt user

--- slurm_scripts/pretrain_debug.sh
+++ slurm_scripts/pretrain_debug.sh
@@ -14,19 +14,19 @@
 cat $0
 
 # ------- User-specific paths --------
-REPO_DIR="/global/homes/i/ibrahime/temp/OmniLearnLightining/"
-# REPO_DIR="/global/homes/j/jobirk/repositories/OmniLearnLightning_dev"
-OUTPUT_DIR="/pscratch/sd/i/ibrahime/checkpoints/pretrained_micro_super_gen/"
-# OUTPUT_DIR=/pscratch/sd/j/jobirk/omnilearned_output/omnilearn_output
-DATASET_PATH="/pscratch/sd/i/ibrahime/datasets/"
-# DATASET_PATH=/pscratch/sd/j/jobirk/omnilearn_datasets/
+# REPO_DIR="/global/homes/i/ibrahime/temp/OmniLearnLightining/"
+REPO_DIR="/global/homes/j/jobirk/repositories/OmniLearnLightning"
+# OUTPUT_DIR="/pscratch/sd/i/ibrahime/checkpoints/pretrained_micro_super_gen/"
+OUTPUT_DIR=/pscratch/sd/j/jobirk/omnilearned_output/omnilearn_output
+# DATASET_PATH="/pscratch/sd/i/ibrahime/datasets/"
+DATASET_PATH=/pscratch/sd/j/jobirk/omnilearn_datasets/
 # ------------------------------------
 
 TOKENIZER_CKPT="${REPO_DIR}/assets/kmeans_model_2025-11-01-00-13-29-Idiotic-Name_withAddInfo_16384_codes.pth"
 DATASET_SIZE=1000000
 VAL_CHECK=1  # Set to one if DATASET_SIZE!=-1 else 5000
 
-TRAINING_CFG_ALL_SIZES="--use_wandb y --wandb_project omnilearned --limit_val_batches=250 --val_check_interval=$VAL_CHECK --dataset_size=$DATASET_SIZE --use_pid y --use_add y --epoch=500  --outdir=$OUTPUT_DIR --path=$DATASET_PATH --shuffle_val_test_indices y --seed_for_initial_shuffling=1603"
+TRAINING_CFG_ALL_SIZES="--user $USER --use_wandb y --wandb_project omnilearned --limit_val_batches=250 --val_check_interval=$VAL_CHECK --dataset_size=$DATASET_SIZE --use_pid y --use_add y --epoch=500  --outdir=$OUTPUT_DIR --path=$DATASET_PATH --shuffle_val_test_indices y --seed_for_initial_shuffling=1603  --pos_encoding_type=sort_descending_in_masked_subset"
 
 ### Micro Model
 TRAINING_CFG_SIZE_SPECIFIC="--num_workers=2 --num_nodes=4 --model_size=micro --lr 1e-3 --weight_decay 0.01 --batch_size=256"
@@ -42,8 +42,10 @@ TRAINING_CFG="$TRAINING_CFG_ALL_SIZES $TRAINING_CFG_SIZE_SPECIFIC"
 # export cmd="python3 train_lightning.py $TRAINING_CFG --mode=mpm --tokenizer_ckpt=$TOKENIZER_CKPT"
 # export cmd="python3 train_lightning.py $TRAINING_CFG --mode=classifier+generator"
 # export cmd="python train_lightning.py $TRAINING_CFG --mode=classifier+mpm --tokenizer_ckpt=$TOKENIZER_CKPT"
+# export cmd="python train_lightning.py $TRAINING_CFG --mode=mpm"
+export cmd="python train_lightning.py $TRAINING_CFG --mode=classifier+mpm"
 # export cmd="python3 train_lightning.py $TRAINING_CFG --mode=generator+mpm --tokenizer_ckpt=$TOKENIZER_CKPT"
-export cmd="python3 train_lightning.py $TRAINING_CFG --mode=pretrain --tokenizer_ckpt=$TOKENIZER_CKPT"
+# export cmd="python3 train_lightning.py $TRAINING_CFG --mode=pretrain --tokenizer_ckpt=$TOKENIZER_CKPT"
 
 # slurm job:
 srun --gpus-per-node 4 shifter bash -c "cd $REPO_DIR/scripts/ && source /opt/conda/bin/activate && export PYTHONPATH=$REPO_DIR:\$PYTHONPATH && $cmd"
