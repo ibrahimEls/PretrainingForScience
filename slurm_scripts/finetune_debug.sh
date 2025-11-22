05a99b14c0bdbc1778d3f2f07b77dc1a36df1b0e
Ibrahim Elsharkawy <ibrahime@login07.chn.perlmutter.nersc.gov>
2025-11-22 07:46:20 -0800

    Debug Script

--- slurm_scripts/finetune_debug.sh
+++ slurm_scripts/finetune_debug.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #SBATCH -A m3246
 #SBATCH -C gpu
-#SBATCH -q regular
-#SBATCH -t 01:00:00
+#SBATCH -q debug
+#SBATCH -t 00:30:00
 #SBATCH -N 4
 #SBATCH --ntasks-per-node=4
 #SBATCH -c 32
@@ -22,41 +22,46 @@ DATASET_PATH="/pscratch/sd/i/ibrahime/datasets/"
 # DATASET_PATH=/pscratch/sd/j/jobirk/omnilearn_datasets/
 # ------------------------------------
 
-PRETRAINED_CKPT="/global/cfs/cdirs/m3246/Omnilearned_Study/Model_Checkpoints/Micro/100k/class_only_val_loss=.5940.ckpt"
+# --------- Config variables ---------
+PRETRAIN_JSON="/global/homes/i/ibrahime/temp/OmniLearnLightining/assets/Paths/Pre-Trained-Model-Paths.json"
+LAYOUT_JSON="/global/homes/i/ibrahime/temp/OmniLearnLightining/assets/Paths/Top-Class-Finetuned-Model-Paths-Template.json"
+STATE_JSON="/global/homes/i/ibrahime/temp/OmniLearnLightining/assets/Paths/Top-Class-Finetuned-Model-Paths-States.json"
+
+DOWNSTREAM_DATASETS="all"
+MODEL_SIZES="micro"
+#PRE_TRAINING_MODES="classifier,generator,mpm,classifier+generator,classifier+mpm,generator+mpm,from_scratch"
+PRE_TRAINING_MODES="classifier,generator,classifier+generator,from_scratch"
+MODE="classifier"
+
 FINETUNING_DATASET="top"
+N_RUNS=3
+BASE_SEED=1603
+NODES=4
 
-TOKENIZER_CKPT="${REPO_DIR}/assets/kmeans_model_2025-11-01-00-13-29-Idiotic-Name_withAddInfo_16384_codes.pth"
-DATASET_SIZE=-1
+CMD_TEMPLATE_FILE="/global/homes/i/ibrahime/temp/OmniLearnLightining/assets/finetune-template.sh"
+# -----------------------------------
 
-# POS_ENCODING_TYPE="sort_descending_all"
-POS_ENCODING_TYPE="sort_descending_in_masked_subset"
-# POS_ENCODING_TYPE="None"
-MASKING_FRACTION=0.4
-
-VAL_CHECK=1
-PATIENCE=10
-
-## RESUME
-TRAINING_CFG_ALL_SIZES="--use_wandb y --wandb_project omnilearned --limit_val_batches=250 --val_check_interval=$VAL_CHECK --dataset=$FINETUNING_DATASET --dataset_size=$DATASET_SIZE --fine_tune y --ckpt=$PRETRAINED_CKPT --use_pid n --use_add n --epoch=100  --outdir=$OUTPUT_DIR --path=$DATASET_PATH --shuffle_val_test_indices y --seed_for_initial_shuffling=1603 --patience=$PATIENCE  --pos_encoding_type=$POS_ENCODING_TYPE --masking_fraction=$MASKING_FRACTION --use_one_cycle y"
-
-## TRAIN FROM SCRATCH
-#TRAINING_CFG_ALL_SIZES="--use_wandb y --wandb_project omnilearned --limit_val_batches=250 --val_check_interval=$VAL_CHECK --dataset=$FINETUNING_DATASET --dataset_size=$DATASET_SIZE --use_pid n --use_add n --epoch=100  --outdir=$OUTPUT_DIR --path=$DATASET_PATH --shuffle_val_test_indices y --seed_for_initial_shuffling=1603 --patience=$PATIENCE  --pos_encoding_type=$POS_ENCODING_TYPE --masking_fraction=$MASKING_FRACTION --use_one_cycle y"
-
-### Micro Model
-TRAINING_CFG_SIZE_SPECIFIC="--num_workers=2 --num_nodes=4 --model_size=micro --lr 1e-3 --weight_decay 0.01 --batch_size=256"
-### Small model
-# TRAINING_CFG_SIZE_SPECIFIC="--num_workers=32 --num_nodes=8 --model_size=small --lr 5e-4 --weight_decay 0.3 --batch_size=128"
-### Medium model
-# TRAINING_CFG_SIZE_SPECIFIC="--num_workers=32 --num_nodes=32 --model_size=medium --lr 5e-6 --weight_decay 0.1 --batch_size=32"
-
-TRAINING_CFG="$TRAINING_CFG_ALL_SIZES $TRAINING_CFG_SIZE_SPECIFIC"
-
-export cmd="python3 train_lightning.py $TRAINING_CFG --mode=classifier"
-
+export cmd="python finetune_lightning.py \
+  --pretrain-json $PRETRAIN_JSON \
+  --layout-json $LAYOUT_JSON \
+  --state-json $STATE_JSON \
+  --downstream-datasets $DOWNSTREAM_DATASETS \
+  --model-sizes $MODEL_SIZES \
+  --pre_training_modes $PRE_TRAINING_MODES \
+  --mode $MODE \
+  --n-runs $N_RUNS \
+  --base-seed $BASE_SEED \
+  --cmd-template-file $CMD_TEMPLATE_FILE\
+  --dataset $FINETUNING_DATASET\
+  --num_nodes=$NODES\
+  --repo_dir=$REPO_DIR\
+  --output_dir=$OUTPUT_DIR\
+  --dataset_dir=$DATASET_PATH"
+ # --dry-run <---- Turn on to see what will be run without running it
 
 # slurm job:
-cd $REPO_DIR/scripts/ && source /opt/conda/bin/activate && export PYTHONPATH=$REPO_DIR:\$PYTHONPATH && $cmd
+srun --gpus-per-node 4 shifter bash -c "cd $REPO_DIR/scripts/ && source /opt/conda/bin/activate && export PYTHONPATH=$REPO_DIR:\$PYTHONPATH && $cmd"
 
 # Interactive use:
 # (specify num_nodes=1 to avoid trying to use multiple nodes in interactive mode)
-#shifter --image=docker:jobirk/omnilearn-lightning:v1.0.0 bash -c "cd $REPO_DIR/scripts/ && source /opt/conda/bin/activate && export PYTHONPATH=$REPO_DIR:\$PYTHONPATH && $cmd --num_nodes=1"
+#shifter --image=docker:jobirk/omnilearn-lightning:v1.0.0 bash -c "cd $REPO_DIR/scripts/ && source /opt/conda/bin/activate && export PYTHONPATH=$REPO_DIR:\$PYTHONPATH && $cmd"
