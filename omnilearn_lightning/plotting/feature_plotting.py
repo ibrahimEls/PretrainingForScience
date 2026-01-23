4fc8d125c8ed04fbd5135b3d72fbcedc088e3dd9
Joschka Birk <joschka.birk@gmail.com>
2026-01-23 14:11:40 +0000

    Add linestyles and ratio references argument in plotting function

--- omnilearn_lightning/plotting/feature_plotting.py
+++ omnilearn_lightning/plotting/feature_plotting.py
@@ -115,7 +115,8 @@ def plot_features(
     decorate_ax_kwargs: dict = {},
     bins_dict: dict = None,
     logscale_features: list[str] = None,
-    colors: list[str] = None,
+    colors: Union[list[str], dict[str, str]] = None,
+    linestyles: Union[list[str], dict[str, str]] = None,
     ax_size: tuple[int, int] = (3, 2),
     ylabel: str = None,
     ratio: bool = False,
@@ -123,6 +124,7 @@ def plot_features(
     normed: bool = True,
     gridspec_hspace: float = 0.0,
     gridspec_wspace: float = 0.4,
+    ratio_references: dict = None,
 ):
     """Plot the features of the constituents or jets.
 
@@ -151,9 +153,14 @@ def plot_features(
         Dict of {name: bins} for the histograms. `name` has to be the same as the keys in `names`.
     logscale_features : list, optional
         List of features to plot in log scale, of "all" to plot all features in log scale.
-    colors : list, optional
+    colors : list or dict, optional
         List of colors for the histograms. Has to have the same length as the number of arrays.
         If shorter, the colors will be repeated.
+        If a dict is given, it has to be of the form {"array_name": "color", ...}.
+    linestyles : list or dict, optional
+        List of linestyles for the histograms. Has to have the same length as the
+        number of arrays. If shorter, the linestyles will be repeated.
+        If a dict is given, it has to be of the form {"array_name": "linestyle", ...}.
     ax_size : tuple, optional
         Size of the axes. Default is (3, 2).
     ylabel : str, optional
@@ -168,6 +175,10 @@ def plot_features(
         Spacing between the rows of the subplot grid. Default is 0.3.
     gridspec_wspace : float, optional
         Spacing between the columns of the subplot grid. Default is 0.2.
+    ratio_references : dict, optional
+        Dict of {name: baseline_array} for the ratio plots. If given, the ratios will be
+        calculated with respect to the baseline arrays. This allows to have multiple
+        references in the ratio plot. Default is None.
     """
 
     default_hist_kwargs = {
@@ -205,6 +216,22 @@ def plot_features(
         if name not in bins_dict:
             bins_dict[name] = histkwargs["bins"]
 
+    # if ratio_references is given, check that all labels are in the dict
+    if ratio and ratio_references is not None:
+        for label in ak_array_dict.keys():
+            if label not in ratio_references:
+                raise ValueError(
+                    f"Label '{label}' not found in ratio_references. "
+                    "Please provide a baseline for each label."
+                )
+        # check that no reference is called before it's plotted itself
+        for i, ref_label in enumerate(ratio_references.values()):
+            if ref_label not in list(ak_array_dict.keys())[: i + 1]:
+                raise ValueError(
+                    f"The array {ref_label} is used as ratio reference before "
+                    "it's plotted itself. Change the order."
+                )
+
     # remove default bins from histkwargs
     histkwargs.pop("bins")
 
@@ -237,8 +264,15 @@ def plot_features(
     legend_handles = []
     legend_labels = []
 
-    base_label, base_array = next(iter(ak_array_dict.items()))
-    base_values_dict = {}
+    if ratio_references is not None:
+        base_label = list(set(ratio_references.values())) if ratio else []
+    else:
+        base_label = next(iter(ak_array_dict.keys()))
+        base_label = [base_label] if ratio else []
+        if len(base_label) > 0:
+            ratio_references = {label: base_label[0] for label in ak_array_dict.keys()}
+
+    base_values_dict = {base_name: {} for base_name in base_label}
 
     # for features where no bins are specified, use the default number of bins but
     # the same range for all plotted histograms
@@ -273,9 +307,37 @@ def plot_features(
         bins_dict[feat] = np.linspace(min_val, max_val, n_bins + 1)
 
     for i_label, (label, ak_array) in enumerate(ak_array_dict.items()):
-        color = colors[i_label] if colors is not None else f"C{i_label}"
+        if colors is not None:
+            color = colors[i_label] if isinstance(colors, list) else colors[label]
+        else:
+            color = f"C{i_label}"
+
+        if linestyles is not None:
+            linestyle = (
+                plot_utils.get_good_linestyles(linestyles[i_label])
+                if isinstance(linestyles, list)
+                else plot_utils.get_good_linestyles(linestyles[label])
+            )
+            if "linestyle" in histkwargs:
+                logger.warning(
+                    "The 'linestyle' keyword argument in histkwargs is being overridden "
+                    "by the linestyles argument."
+                )
+            histkwargs["linestyle"] = linestyle
+        else:
+            histkwargs["linestyle"] = "solid"
+
         legend_labels.append(label)
         for i, (feat, feat_label) in enumerate(names.items()):
+            # if multiple ratio references are use: draw horizontal line
+            if ratio and i_label == 0 and len(base_label) > 1:
+                axarr_ratio[i].axhline(
+                    1,
+                    ls="--",
+                    color="dimgray",
+                    linewidth=histkwargs.get("linewidth", 1),
+                )
+
             if not hasattr(ak_array, feat):
                 logger.info(f"Feature '{feat}' not found in array '{label}', skipping.")
                 continue
@@ -343,13 +405,17 @@ def plot_features(
                 )
 
             if ratio:
-                if label == base_label:
-                    base_values_dict[feat] = hist
+                if label in base_label:
+                    base_values_dict[label][feat] = hist
+                    # if multiple baselines are given, don't plot the ratio for
+                    # the baselines, but a horizontal dashed line at 1
+                    if len(base_label) > 1:
+                        continue
 
                 # calculate the ratio of the histogram to the base histogram
                 ratio_values, ratio_unc = hist_ratio(
                     numerator=hist,
-                    denominator=base_values_dict[feat],
+                    denominator=base_values_dict[ratio_references[label]][feat],
                     numerator_unc=unc,
                     step=False,
                 )
@@ -362,6 +428,7 @@ def plot_features(
                     where="pre",
                     color=color,
                     linewidth=histkwargs.get("linewidth", 1),
+                    linestyle=histkwargs.get("linestyle", "solid"),
                 )
 
                 # calculate the uncertainty band for the ratio
