571f5da9467fa1cc29274e8dbbdd8d452f621055
Joschka Birk <joschka.birk@gmail.com>
2026-02-25 09:38:00 +0000

    Remove the `on_load_checkpoint` method cause it's broken with the current model anyways

--- omnilearn_lightning/model.py
+++ omnilearn_lightning/model.py
@@ -755,25 +755,25 @@ class PETLightning(LightningModule):
                 },
             }
 
-    def on_load_checkpoint(self, checkpoint: dict) -> None:
-        # if fine_tuning a pretrained body, filter shapes
-        # NOTE: I actually think that this method is incompatible with the current
-        # repo, cause our models don't have the key `body` in the state dict.
-        # Instead, it should be `model.body`
-        if self.fine_tune and self.ckpt_loaded:
-            print(
-                "[WARNING] both fine_tune and ckpt_loaded are set. This will "
-                "load the checkpoint with strict=False, which may lead to unexpected "
-                "behavior if the checkpoint is not compatible with the current model "
-                "architecture."
-            )
-            ck = torch.load(self.ckpt_loaded, map_location="cpu")
-            body_sd = ck["body"]
-            self.model.body.load_state_dict(body_sd, strict=False)
+    # def on_load_checkpoint(self, checkpoint: dict) -> None:
+    #     # if fine_tuning a pretrained body, filter shapes
+    #     # NOTE: I actually think that this method is incompatible with the current
+    #     # repo, cause our models don't have the key `body` in the state dict.
+    #     # Instead, it should be `model.body`
+    #     if self.fine_tune and self.ckpt_loaded:
+    #         print(
+    #             "[WARNING] both fine_tune and ckpt_loaded are set. This will "
+    #             "load the checkpoint with strict=False, which may lead to unexpected "
+    #             "behavior if the checkpoint is not compatible with the current model "
+    #             "architecture."
+    #         )
+    #         ck = torch.load(self.ckpt_loaded, map_location="cpu")
+    #         body_sd = ck["body"]
+    #         self.model.body.load_state_dict(body_sd, strict=False)
 
-            # classifier_head / generator_head if present...
-            for head in ("classifier_head", "generator_head"):
-                if head in ck and getattr(self.model, head[:-5], None) is not None:
-                    getattr(self.model, head[:-5]).load_state_dict(
-                        ck[head], strict=False
-                    )
+    #         # classifier_head / generator_head if present...
+    #         for head in ("classifier_head", "generator_head"):
+    #             if head in ck and getattr(self.model, head[:-5], None) is not None:
+    #                 getattr(self.model, head[:-5]).load_state_dict(
+    #                     ck[head], strict=False
+    #                 )
