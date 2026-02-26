f58882e15ae2c81a9f0c7e93545a88ea8fed836d
Joschka Birk <joschka.birk@gmail.com>
2026-02-26 12:57:30 +0000

    Add option to set epoch frequency for eval check (to prevent validation time > training time with small datasets)

--- scripts/train_lightning.py
+++ scripts/train_lightning.py
@@ -92,6 +92,15 @@ def main():
         default=None,
         help="Number of training batches between each validation check. If <1.0, this is a fraction of the total training batches.",
     )
+    parser.add_argument(
+        "--check_val_every_n_epoch",
+        type=int,
+        default=None,
+        help="Number of training epochs between each validation check. If set, this "
+        "overrides val_check_interval for epoch-based validation checking "
+        "(i.e., validation will be checked every N epochs regardless of the "
+        "number of training batches).",
+    )
     parser.add_argument("--shuffle_val_test_indices", type=str2bool, default=False)
     parser.add_argument("--seed_for_initial_shuffling", type=int, default=None)
     parser.add_argument(
@@ -619,6 +628,9 @@ def main():
         enable_progress_bar=(args.num_nodes == 1),
         limit_val_batches=args.limit_val_batches,
         val_check_interval=args.val_check_interval,
+        check_val_every_n_epoch=args.check_val_every_n_epoch
+        if args.val_check_interval is None
+        else None,
     )
 
     # Training
